bf3cc6f5e16ca2975c405f9fc13c7337eb78ef8f
Adam <adam.haldas@gmail.com>
2021-11-11 22:32:40 +0100

    add some socials and footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer>
-      <Container bg="dark">
+      <Container bg='dark'>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; sklep fhuj</Col>
+          <Col className='text-center py-3'> Zaliczenie python WSB</Col>
+          <Col className='text-center py-3'>
+            {' '}
+            <FontAwesomeIcon icon={faFacebook} className={'socialsIcons'} />
+            <FontAwesomeIcon icon={faInstagram} className={'socialsIcons'} />
+            <FontAwesomeIcon icon={faGithub} className={'socialsIcons'} />
+          </Col>
+          <Col className='text-center py-3 '>Copyright &copy; 2021</Col>
         </Row>
       </Container>
     </footer>
